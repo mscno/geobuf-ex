2a4454519081688453b1da2ed1ee6be60bf5e04a
mscno <msc@parap.ly>
2024-02-29 12:28:26 +0100

    bump deps and bump to 1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Geobuf.MixProject do
   def project do
     [
       app: :geobuf,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
